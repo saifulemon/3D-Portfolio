5b017068ce0a8d2aa87e1847d5322906c742cfda
saifulemon <463saiful0@gmail.com>
2023-03-13 01:03:59 +0600

    added who component in hero section

--- src/components/Who.jsx
+++ src/components/Who.jsx
@@ -1,15 +1,85 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
-`
+  display: flex;
+  justify-content: center;
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+  width: 1150px;
+  display: flex;
+  justify-content: space-between;
+  padding: 10px 0;
+`;
+
+const Left = styled.div`
+  flex: 1;
+`;
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
+`;
+const Title = styled.h1`
+  font-size: 74px;
+  margin: 0;
+`;
+const WhatWeDo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+const Line = styled.img`
+  height: 10px;
+`;
+const Subtitle = styled.h2`
+  color: #da4ea2;
+  margin: 0;
+`;
+const Desc = styled.p`
+  font-size: 24px;
+  color: lightgray;
+  margin: 0;
+`;
+const Button = styled.button`
+  background-color: #da4ea2;
+  color: white;
+  font-weight: 500;
+  width: 120px;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
 
 const Who = () => {
-  return (
-    <Section>Who</Section>
-  )
-}
+    return (
+        <Section>
+           <Container>
+            <Left>
+              {/* 3D model */}
+              {/* <Img src="./img/moon.png"/> */}
+            </Left>
+            <Right>
+            <Title>Think outside the square space</Title>
+              <WhatWeDo>
+                <Line src="./img/line.png"/>
+                <Subtitle>Who we Are</Subtitle>
+              </WhatWeDo>
+              <Desc>a creative group of designers and developers with a passion for the arts.</Desc>
+              <Button>See our works</Button>
+            </Right>
+           </Container>
+        </Section>
+    );
+};
 
-export default Who
+export default Who;
